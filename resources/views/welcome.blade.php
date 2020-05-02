0f2faeb48896e575350c67fbbd0085a5023171bb
tri angga utama <trianggategarutama@gmail.com>
2020-05-03 00:56:22 +0800

    bersih json

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -425,6 +425,200 @@
                 </div><!-- media -->
               </div>
             </div><!-- col -->
+            <div class="col-md-6 col-xl-4 mg-t-10">
+              <div class="card ht-100p">
+                <div class="card-header d-flex align-items-center justify-content-between">
+                  <h6 class="mg-b-0">Transaction History</h6>
+                  <div class="d-flex tx-18">
+                    <a href="" class="link-03 lh-0"><i class="icon ion-md-refresh"></i></a>
+                    <a href="" class="link-03 lh-0 mg-l-10"><i class="icon ion-md-more"></i></a>
+                  </div>
+                </div>
+                <ul class="list-group list-group-flush tx-13">
+                  <li class="list-group-item d-flex pd-sm-x-20">
+                    <div class="avatar d-none d-sm-block"><span class="avatar-initial rounded-circle bg-teal"><i class="icon ion-md-checkmark"></i></span></div>
+                    <div class="pd-sm-l-10">
+                      <p class="tx-medium mg-b-0">Payment from #10322</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Mar 21, 2019, 3:30pm</small>
+                    </div>
+                    <div class="mg-l-auto text-right">
+                      <p class="tx-medium mg-b-0">+ $250.00</p>
+                      <small class="tx-12 tx-success mg-b-0">Completed</small>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-sm-x-20">
+                    <div class="avatar d-none d-sm-block"><span class="avatar-initial rounded-circle bg-indigo op-5"><i class="icon ion-md-return-left"></i></span></div>
+                    <div class="pd-sm-l-10">
+                      <p class="tx-medium mg-b-2">Process refund to #00910</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Mar 21, 2019, 1:00pm</small>
+                    </div>
+                    <div class="mg-l-auto text-right">
+                      <p class="tx-medium mg-b-2">-$16.50</p>
+                      <small class="tx-12 tx-success mg-b-0">Completed</small>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-sm-x-20">
+                    <div class="avatar d-none d-sm-block"><span class="avatar-initial rounded-circle bg-orange op-5"><i class="icon ion-md-bus"></i></span></div>
+                    <div class="pd-sm-l-10">
+                      <p class="tx-medium mg-b-2">Process delivery to #44333</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Mar 20, 2019, 11:40am</small>
+                    </div>
+                    <div class="mg-l-auto text-right">
+                      <p class="tx-medium mg-b-2">3 Items</p>
+                      <small class="tx-12 tx-info mg-b-0">For pickup</small>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-sm-x-20">
+                    <div class="avatar d-none d-sm-block"><span class="avatar-initial rounded-circle bg-teal"><i class="icon ion-md-checkmark"></i></span></div>
+                    <div class="pd-sm-l-10">
+                      <p class="tx-medium mg-b-0">Payment from #023328</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Mar 20, 2019, 10:30pm</small>
+                    </div>
+                    <div class="mg-l-auto text-right">
+                      <p class="tx-medium mg-b-0">+ $129.50</p>
+                      <small class="tx-12 tx-success mg-b-0">Completed</small>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-sm-x-20">
+                    <div class="avatar d-none d-sm-block"><span class="avatar-initial rounded-circle bg-gray-400"><i class="icon ion-md-close"></i></span></div>
+                    <div class="pd-sm-l-10">
+                      <p class="tx-medium mg-b-0">Payment failed from #087651</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Mar 19, 2019, 12:54pm</small>
+                    </div>
+                    <div class="mg-l-auto text-right">
+                      <p class="tx-medium mg-b-0">$150.00</p>
+                      <small class="tx-12 tx-danger mg-b-0">Declined</small>
+                    </div>
+                  </li>
+                </ul>
+                <div class="card-footer text-center tx-13">
+                  <a href="" class="link-03">View All Transactions <i class="icon ion-md-arrow-down mg-l-5"></i></a>
+                </div><!-- card-footer -->
+              </div><!-- card -->
+            </div>
+            <div class="col-md-6 col-xl-4 mg-t-10">
+              <div class="card ht-100p">
+                <div class="card-header d-flex align-items-center justify-content-between">
+                  <h6 class="mg-b-0">New Customers</h6>
+                  <div class="d-flex align-items-center tx-18">
+                    <a href="" class="link-03 lh-0"><i class="icon ion-md-refresh"></i></a>
+                    <a href="" class="link-03 lh-0 mg-l-10"><i class="icon ion-md-more"></i></a>
+                  </div>
+                </div>
+                <ul class="list-group list-group-flush tx-13">
+                  <li class="list-group-item d-flex pd-sm-x-20">
+                    <div class="avatar"><span class="avatar-initial rounded-circle bg-gray-600">s</span></div>
+                    <div class="pd-l-10">
+                      <p class="tx-medium mg-b-0">Socrates Itumay</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Customer ID#00222</small>
+                    </div>
+                    <div class="mg-l-auto d-flex align-self-center">
+                      <nav class="nav nav-icon-only">
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="mail"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="slash"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="user"></i></a>
+                        <a href="" class="nav-link d-sm-none"><i data-feather="more-vertical"></i></a>
+                      </nav>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-x-20">
+                    <div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div>
+                    <div class="pd-l-10">
+                      <p class="tx-medium mg-b-0">Reynante Labares</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Customer ID#00221</small>
+                    </div>
+                    <div class="mg-l-auto d-flex align-self-center">
+                      <nav class="nav nav-icon-only">
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="mail"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="slash"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="user"></i></a>
+                        <a href="" class="nav-link d-sm-none"><i data-feather="more-vertical"></i></a>
+                      </nav>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-x-20">
+                    <div class="avatar"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div>
+                    <div class="pd-l-10">
+                      <p class="tx-medium mg-b-0">Marianne Audrey</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Customer ID#00220</small>
+                    </div>
+                    <div class="mg-l-auto d-flex align-self-center">
+                      <nav class="nav nav-icon-only">
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="mail"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="slash"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="user"></i></a>
+                        <a href="" class="nav-link d-sm-none"><i data-feather="more-vertical"></i></a>
+                      </nav>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-x-20">
+                    <div class="avatar"><span class="avatar-initial rounded-circle bg-indigo op-5">o</span></div>
+                    <div class="pd-l-10">
+                      <p class="tx-medium mg-b-0">Owen Bongcaras</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Customer ID#00219</small>
+                    </div>
+                    <div class="mg-l-auto d-flex align-self-center">
+                      <nav class="nav nav-icon-only">
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="mail"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="slash"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="user"></i></a>
+                        <a href="" class="nav-link d-sm-none"><i data-feather="more-vertical"></i></a>
+                      </nav>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex pd-x-20">
+                    <div class="avatar"><span class="avatar-initial rounded-circle bg-primary op-5">k</span></div>
+                    <div class="pd-l-10">
+                      <p class="tx-medium mg-b-0">Kirby Avendula</p>
+                      <small class="tx-12 tx-color-03 mg-b-0">Customer ID#00218</small>
+                    </div>
+                    <div class="mg-l-auto d-flex align-self-center">
+                      <nav class="nav nav-icon-only">
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="mail"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="slash"></i></a>
+                        <a href="" class="nav-link d-none d-sm-block"><i data-feather="user"></i></a>
+                        <a href="" class="nav-link d-sm-none"><i data-feather="more-vertical"></i></a>
+                      </nav>
+                    </div>
+                  </li>
+                </ul>
+                <div class="card-footer text-center tx-13">
+                  <a href="" class="link-03">View More Customers <i class="icon ion-md-arrow-down mg-l-5"></i></a>
+                </div><!-- card-footer -->
+              </div><!-- card -->
+            </div>
+            <div class="col-md-6 col-xl-4 mg-t-10">
+              <div class="card ht-lg-100p">
+                <div class="card-header d-flex align-items-center justify-content-between">
+                  <h6 class="mg-b-0">Real-Time Sales</h6>
+                  <ul class="list-inline d-flex mg-b-0">
+                    <li class="list-inline-item d-flex align-items-center">
+                      <span class="d-block wd-10 ht-10 bg-df-2 rounded mg-r-5"></span>
+                      <span class="tx-sans tx-uppercase tx-10 tx-medium tx-color-03">Today</span>
+                    </li>
+                    <li class="list-inline-item d-flex align-items-center mg-l-10">
+                      <span class="d-block wd-10 ht-10 bg-df-3 rounded mg-r-5"></span>
+                      <span class="tx-sans tx-uppercase tx-10 tx-medium tx-color-03">Yesterday</span>
+                    </li>
+                  </ul>
+                </div><!-- card-header -->
+                <div class="card-body pd-b-0">
+                  <div class="row mg-b-20">
+                    <div class="col">
+                      <h5 class="tx-normal tx-rubik tx-spacing--1 mg-b-10">$150,200 <small class="tx-11 tx-success letter-spacing--2"><i class="icon ion-md-arrow-up"></i> 0.20%</small></h5>
+                      <p class="tx-10 tx-uppercase tx-spacing-1 tx-medium tx-color-03">Total Sales</p>
+                    </div>
+                    <div class="col">
+                      <h5 class="tx-normal tx-rubik tx-spacing--1 mg-b-10">$21,880 <small class="tx-11 tx-danger letter-spacing--2"><i class="icon ion-md-arrow-down"></i> 1.04%</small></h5>
+                      <p class="tx-10 tx-uppercase tx-spacing-1 tx-medium tx-color-03">Avg. Sales Per Day</p>
+                    </div>
+                  </div><!-- row -->
+                  <div class="chart-five">
+                    <div><canvas id="chartBar1"></canvas></div>
+                  </div>
+                </div><!-- card-body -->
+              </div>
+            </div>
           </div><!-- row -->
         </div><!-- container -->
       </div>
@@ -448,8 +642,6 @@
     <!-- append theme customizer -->
     <script src="{{asset('admin/lib/js-cookie/js.cookie.js')}}"></script>
     
-    <script src="{{asset('admin/assets/js/dashforge.settings.js')}}"></script>
-
     <script>
       $(function(){
         'use strict'
