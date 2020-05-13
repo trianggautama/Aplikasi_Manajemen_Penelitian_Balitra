a744bb90c91e02a27c2b15181592d3ec1511c01f
muhammadasadbastomi <tomycules@gmail.com>
2020-05-13 09:47:26 +0800

    replace store action into pembimbingStore

--- resources/views/admin/pembimbing/index.blade.php
+++ resources/views/admin/pembimbing/index.blade.php
@@ -2,45 +2,47 @@
 
 @section('content')
 <div class="content-body">
-        <div class="container pd-x-0">
-          <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
-            <div>
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb breadcrumb-style1 mg-b-10">
-                  <li class="breadcrumb-item"><a href="#">Pegawai</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Pembimbing Lapangan</li>
-                </ol>
-              </nav>
-              <h4 class="mg-b-0 tx-spacing--1">Pembimbing Lapangan</h4>
-            </div>
-            <div class="d-none d-md-block">
-              <button class="btn btn-sm pd-x-15 btn-white btn-uppercase mg-l-5"><i data-feather="printer" class="wd-10 mg-r-5"></i> Print</button>
-              <a class="btn btn-sm pd-x-15 btn-dark btn-uppercase mg-l-5" href="#modal2" data-toggle="modal"><i data-feather="plus" class="wd-10 mg-r-5"></i> tambah Data</a>
-            </div>
-          </div>
+  <div class="container pd-x-0">
+    <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
+      <div>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb breadcrumb-style1 mg-b-10">
+            <li class="breadcrumb-item"><a href="#">Pegawai</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Pembimbing Lapangan</li>
+          </ol>
+        </nav>
+        <h4 class="mg-b-0 tx-spacing--1">Pembimbing Lapangan</h4>
+      </div>
+      <div class="d-none d-md-block">
+        <button class="btn btn-sm pd-x-15 btn-white btn-uppercase mg-l-5"><i data-feather="printer"
+            class="wd-10 mg-r-5"></i> Print</button>
+        <a class="btn btn-sm pd-x-15 btn-dark btn-uppercase mg-l-5" href="#modal2" data-toggle="modal"><i
+            data-feather="plus" class="wd-10 mg-r-5"></i> tambah Data</a>
+      </div>
+    </div>
 
-          <div class="row row-xs">
-            <div class="col-md-12 col-xl-12 mg-t-10">
-            <div class="card card-body ">
-            <div data-label="Example" class="df-example demo-table">
-          <table id="dataTable" class="table text-center">
-            <thead>
+    <div class="row row-xs">
+      <div class="col-md-12 col-xl-12 mg-t-10">
+        <div class="card card-body ">
+          <div data-label="Example" class="df-example demo-table">
+            <table id="dataTable" class="table text-center">
+              <thead>
                 <tr>
-                    <th>No</th>
-                    <th>NIP</th>
-                    <th>Nama</th>
-                    <th>Jabatan</th>
-                    <th>Username</th>
-                    <th>Aksi</th>
+                  <th>No</th>
+                  <th>NIP</th>
+                  <th>Nama</th>
+                  <th>Jabatan</th>
+                  <th>Username</th>
+                  <th>Aksi</th>
                 </tr>
-            </thead> 
-            <tbody>
-              @foreach($data as $d)
+              </thead>
+              <tbody>
+                @foreach($data as $d)
                 <tr>
-                  <td>{{$d->loopiteration}}</td>
-                  <td>{{$d->data_pribadi->nip}}</td>
+                  <td>{{$loop->iteration}}</td>
+                  <td>{{$d->data_personal->NIP}}</td>
                   <td>{{$d->nama}}</td>
-                  <td>{{$d->data_pribadi->jabatan}}</td>
+                  <td>{{$d->data_personal->jabatan}}</td>
                   <td>{{$d->username}}</td>
                   <td>
                     <a href="{{Route('userEdit',['uuid'=>$d->uuid])}}" class="btn btn-primary btn-icon">
@@ -52,27 +54,27 @@
                   </td>
                 </tr>
                 @endforeach
-            </tbody>
-        </table>
-        </div><!-- df-example -->
-              </div>
-            </div>
-          </div><!-- row -->
-        </div><!-- container -->
+              </tbody>
+            </table>
+          </div><!-- df-example -->
+        </div>
       </div>
+    </div><!-- row -->
+  </div><!-- container -->
+</div>
 
-      <!-- modal -->
-      <div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content tx-14">
-          <div class="modal-header">
-            <h6 class="modal-title" id="exampleModalLabel2">Modal Title</h6>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-          <form action="{{route('userStore')}}" method="post" enctype="multipart/form-data">
+<!-- modal -->
+<div class="modal fade" id="modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content tx-14">
+      <div class="modal-header">
+        <h6 class="modal-title" id="exampleModalLabel2">Modal Title</h6>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="{{route('pembimbingStore')}}" method="post" enctype="multipart/form-data">
           @csrf
           <div class="form-group">
             <label for="Nama">Nama</label>
@@ -95,13 +97,13 @@
               <div class="form-group">
                 <label for="Nama">Tempat Lahir</label>
                 <input type="text" name="tempat_lahir" class="form-control" placeholder="Tempat Lahir">
-            </div>
+              </div>
             </div>
             <div class="col-md-6">
               <div class="form-group">
                 <label for="Nama">Tanggal Lahir</label>
                 <input type="date" name="tanggal_lahir" class="form-control" placeholder="Tanggal Lahir">
-            </div>
+              </div>
             </div>
           </div>
           <div class="form-group">
@@ -110,15 +112,15 @@
           </div>
           <div class="row">
             <div class="col-md-6">
-                <div class="form-group">
-                  <label for="Nama">Username</label>
-                  <input type="text" name="username" class="form-control" placeholder="Username">
+              <div class="form-group">
+                <label for="Nama">Username</label>
+                <input type="text" name="username" class="form-control" placeholder="Username">
               </div>
             </div>
             <div class="col-md-6">
-                <div class="form-group">
-                  <label for="Nama">Password</label>
-                  <input type="password" name="password" class="form-control" placeholder="username">
+              <div class="form-group">
+                <label for="Nama">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="username">
               </div>
             </div>
           </div>
@@ -128,13 +130,13 @@
               Simpan</button>
           </div>
         </form>
-        </div>
       </div>
     </div>
-@endsection
-@section('scripts')
-    <script>
-        $(function(){
+  </div>
+  @endsection
+  @section('scripts')
+  <script>
+    $(function(){
         'use strict'
 
         $('#dataTable').DataTable({
@@ -145,5 +147,5 @@
           }
         });
       });
-    </script>
-@endsection
+  </script>
+  @endsection
