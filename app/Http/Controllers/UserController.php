ec1d38554b141c453dc099e72926d4f215cce4e6
muhammadasadbastomi <tomycules@gmail.com>
2020-05-13 09:25:24 +0800

    add function update and destroy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = User::OrderBy('id', 'Desc')->get();
+        $data = User::OrderBy('id', 'Desc')->get();
 
         return view('admin.user.index', compact('data'));
     }
@@ -47,8 +47,57 @@ class UserController extends Controller
         return redirect()->route('userIndex')->with('success', 'Berhasil menyimpan data');
     }
 
-    public function edit($uuid){
-        $data = null;
-        return view('admin.user.edit',compact('data'));
+    public function edit($uuid)
+    {
+        $data = User::where('uuid', $uuid)->first();
+        return view('admin.user.edit', compact('data'));
+    }
+
+    public function update(Request $request, $uuid)
+    {
+        $user = user::where('uuid', $uuid)->first();
+
+        $user->nama = $request->nama;
+        $user->username = $request->username;
+        if (isset($request->password)) {
+            $user->password = Hash::make($request->password);
+        } else {
+            $user->password = $user->password;
+        }
+        if ($request->foto != null) {
+            $img = $request->file('foto');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $request->nama;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('images/user', $foto);
+            $user->foto = $foto;
+        } else {
+            $user->foto = $user->foto;
+        }
+
+        $user->update();
+
+        $personal = Data_personal::where('user_id', $user->id)->first();
+
+        $personal->user_id = $user->id;
+        $personal->NIP = $request->NIP;
+        $personal->jabatan = $request->jabatan;
+        $personal->no_hp = $request->no_hp;
+        $personal->tempat_lahir = $request->tempat_lahir;
+        $personal->tanggal_lahir = $request->tanggal_lahir;
+        $personal->alamat = $request->alamat;
+
+        $personal->update();
+
+        return redirect()->route('userIndex')->with('success', 'Berhasil mengubah data');
+
+    }
+
+    public function destroy($uuid)
+    {
+        $user = User::where('uuid', $uuid)->first()->delete();
+
+        return redirect()->route('userIndex')->with('success', 'Berhasil menghapus data');
+
     }
 }
