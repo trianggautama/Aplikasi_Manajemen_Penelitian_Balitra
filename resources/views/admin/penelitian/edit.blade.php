2528b4831c4c16fc4c73b2ca8bc2f5a05d5a68e0
muhammadasadbastomi <tomycules@gmail.com>
2020-06-01 02:57:14 +0800

    backend penelitian edit view

--- resources/views/admin/penelitian/edit.blade.php
+++ resources/views/admin/penelitian/edit.blade.php
@@ -2,65 +2,71 @@
 
 @section('content')
 <div class="content-body">
-        <div class="container pd-x-0">
-          <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
-            <div>
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb breadcrumb-style1 mg-b-10">
-                  <li class="breadcrumb-item"><a href="#">Penelitian</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Edit</li>
-                </ol>
-              </nav>
-              <h4 class="mg-b-0 tx-spacing--1">Edit Penelitian</h4>
-            </div>
-          </div>
+  <div class="container pd-x-0">
+    <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
+      <div>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb breadcrumb-style1 mg-b-10">
+            <li class="breadcrumb-item"><a href="#">Penelitian</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Edit</li>
+          </ol>
+        </nav>
+        <h4 class="mg-b-0 tx-spacing--1">Edit Penelitian</h4>
+      </div>
+    </div>
 
-          <div class="row row-xs">
-            <div class="col-md-12 col-xl-12 mg-t-10">
-                <div class="card">
-                <div class="card-body">
-                <form action="" method="post">
-                    {{method_field('PUT') }}
-                    @csrf
-          <div class="form-group">
-            <label for="Nama">Pembimbing</label>
-            <select name="" id="" class="form-control">
-                <option value="">-- pilihan ambil dari pembimbing --</option>
-                <option value="">John Doe </option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label for="Nama">Objek Penelitian</label>
-            <select name="" id="" class="form-control">
-                <option value="">-- pilihan ambil dari objek Penelitian --</option>
-                <option value="">Jagung </option>
-            </select>
-          </div>
-            <div class="form-group">
+    <div class="row row-xs">
+      <div class="col-md-12 col-xl-12 mg-t-10">
+        <div class="card">
+          <div class="card-body">
+            <form action="" method="post">
+              {{method_field('PUT') }}
+              @csrf
+              <div class="form-group">
+                <label for="Nama">Pembimbing</label>
+                <select name="user_id" id="" class="form-control">
+                  <option value="">-- Pilih Pembimbing --</option>
+                  @foreach($pembimbing as $d)
+                  <option value="{{$d->id}}" {{$d->id == $data->user_id ? 'selected' : ''}}>{{$d->nama}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="Nama">Objek Penelitian</label>
+                <select name="objek_penelitian_id" id="" class="form-control">
+                  <option value="">-- Pilih Objek Penelitian --</option>
+                  @foreach($objek as $d)
+                  <option value="{{$d->id}}" {{$d->id == $data->objek_penelitian_id ? 'selected' : ''}}>{{$d->nama}}
+                  </option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group">
                 <label for="Nama">Uraian</label>
-                <textarea name="uraian" id="" class="form-control"></textarea>
+                <textarea name="uraian" id="" class="form-control">{{$data->uraian}}</textarea>
               </div>
               <div class="form-group">
                 <label for="Nama">Estimasi (Hari Kerja)</label>
-                <input type="number" name="password" class="form-control" >
+                <input type="number" name="estimasi" value="{{$data->estimasi}}" class="form-control">
               </div>
-                    </div>
-                    <div class="card-footer text-right">
-                        <button type="button" class="btn btn-secondary tx-13" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary tx-13"><i data-feather="save" class="wd-10 mg-r-5"></i> Simpan</button>
-                    </div>
-                    </form>
-                </div>
-            </div>
-          </div><!-- row -->
-        </div><!-- container -->
+          </div>
+          <div class="card-footer text-right">
+            <button type="button" class="btn btn-secondary tx-13" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary tx-13"><i data-feather="save" class="wd-10 mg-r-5"></i>
+              Simpan</button>
+          </div>
+          </form>
+        </div>
       </div>
+    </div><!-- row -->
+  </div><!-- container -->
+</div>
 
 
 @endsection
 @section('scripts')
-    <script>
-        $(function(){
+<script>
+  $(function(){
         'use strict'
 
         $('#dataTable').DataTable({
@@ -71,5 +77,5 @@
           }
         });
       });
-    </script>
+</script>
 @endsection
