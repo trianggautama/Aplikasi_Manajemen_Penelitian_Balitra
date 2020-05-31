5392cd85695613a06b8204ef0f35d10c2d4108c8
muhammadasadbastomi <tomycules@gmail.com>
2020-06-01 00:33:40 +0800

    minor

--- resources/views/admin/penelitian/index.blade.php
+++ resources/views/admin/penelitian/index.blade.php
@@ -2,64 +2,65 @@
 
 @section('content')
 <div class="content-body">
-        <div class="container pd-x-0">
-          <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
-            <div>
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb breadcrumb-style1 mg-b-10">
-                  <li class="breadcrumb-item"><a href="#">Penelitian</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Data</li>
-                </ol>
-              </nav>
-              <h4 class="mg-b-0 tx-spacing--1">Data Penelitian </h4>
-            </div>
-            <div class="d-none d-md-block">
-                <a class="btn btn-sm pd-x-15 btn-dark btn-uppercase mg-l-5" href="#modal2" data-toggle="modal"><i
-                    data-feather="plus" class="wd-10 mg-r-5"></i> tambah Data</a>
-                <button class="btn btn-sm pd-x-15 btn-white btn-uppercase mg-l-5"><i data-feather="printer" class="wd-10 mg-r-5"></i> Print</button>
-            </div>
-          </div>
-
-          <div class="row row-xs">
-            <div class="col-md-12 col-xl-12 mg-t-10">
-            <div class="card card-body ">
-            <div data-label="Example" class="df-example demo-table">
-          <table id="dataTable" class="table text-center">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Peneliti</th>
-                    <th>Pembimbing</th>
-                    <th>Estimasi</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Agus</td>
-                    <td>Iman</td>
-                    <td>40 Hari Kerja</td>
-                    <td>
-                        <a href="{{Route('permohonanDetail')}}" class="btn btn-default btn-secondary btn-sm p-2">
-                            <i data-feather="info"></i>
-                        </a>
-                        <a href="{{Route('penelitianEdit')}}" class="btn btn-primary btn-icon">
-                            <i data-feather="edit"></i>
-                        </a>
-                        <button type="button" class="btn btn-danger btn-icon">
-                            <i data-feather="delete"></i>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        </div><!-- df-example -->
-              </div>
-            </div>
-          </div><!-- row -->
-        </div><!-- container -->
+  <div class="container pd-x-0">
+    <div class="d-sm-flex align-items-center justify-content-between mg-b-20 mg-lg-b-25 mg-xl-b-30">
+      <div>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb breadcrumb-style1 mg-b-10">
+            <li class="breadcrumb-item"><a href="#">Penelitian</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Data</li>
+          </ol>
+        </nav>
+        <h4 class="mg-b-0 tx-spacing--1">Data Penelitian </h4>
       </div>
+      <div class="d-none d-md-block">
+        <a class="btn btn-sm pd-x-15 btn-dark btn-uppercase mg-l-5" href="#modal2" data-toggle="modal"><i
+            data-feather="plus" class="wd-10 mg-r-5"></i> tambah Data</a>
+        <button class="btn btn-sm pd-x-15 btn-white btn-uppercase mg-l-5"><i data-feather="printer"
+            class="wd-10 mg-r-5"></i> Print</button>
+      </div>
+    </div>
+
+    <div class="row row-xs">
+      <div class="col-md-12 col-xl-12 mg-t-10">
+        <div class="card card-body ">
+          <div data-label="Example" class="df-example demo-table">
+            <table id="dataTable" class="table text-center">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Peneliti</th>
+                  <th>Pembimbing</th>
+                  <th>Estimasi</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Agus</td>
+                  <td>Iman</td>
+                  <td>40 Hari Kerja</td>
+                  <td>
+                    <a href="{{Route('penelitianDetail')}}" class="btn btn-default btn-secondary btn-sm p-2">
+                      <i data-feather="info"></i>
+                    </a>
+                    <a href="{{Route('penelitianEdit')}}" class="btn btn-primary btn-icon">
+                      <i data-feather="edit"></i>
+                    </a>
+                    <button type="button" class="btn btn-danger btn-icon">
+                      <i data-feather="delete"></i>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div><!-- df-example -->
+        </div>
+      </div>
+    </div><!-- row -->
+  </div><!-- container -->
+</div>
 
 <!-- modal -->
 <div class="modal fade bd-example-modal-lg" id="modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2"
@@ -78,32 +79,32 @@
           <div class="form-group">
             <label for="Nama">Peneliti</label>
             <select name="" id="" class="form-control">
-                <option value="">-- pilihan ambil dari peneliti --</option>
-                <option value="">John Doe </option>
+              <option value="">-- pilihan ambil dari peneliti --</option>
+              <option value="">John Doe </option>
             </select>
           </div>
           <div class="form-group">
             <label for="Nama">Pembimbing</label>
             <select name="" id="" class="form-control">
-                <option value="">-- pilihan ambil dari pembimbing --</option>
-                <option value="">John Doe </option>
+              <option value="">-- pilihan ambil dari pembimbing --</option>
+              <option value="">John Doe </option>
             </select>
           </div>
           <div class="form-group">
             <label for="Nama">Permohonan</label>
             <select name="" id="" class="form-control">
-                <option value="">-- pilihan ambil dari permohonan yang disetujui --</option>
-                <option value="">Jagung </option>
+              <option value="">-- pilihan ambil dari permohonan yang disetujui --</option>
+              <option value="">Jagung </option>
             </select>
           </div>
-            <div class="form-group">
-                <label for="Nama">Uraian</label>
-                <textarea name="uraian" id="" class="form-control"></textarea>
-              </div>
-              <div class="form-group">
-                <label for="Nama">Estimasi (Hari Kerja)</label>
-                <input type="number" name="password" class="form-control" >
-              </div>
+          <div class="form-group">
+            <label for="Nama">Uraian</label>
+            <textarea name="uraian" id="" class="form-control"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="Nama">Estimasi (Hari Kerja)</label>
+            <input type="number" name="password" class="form-control">
+          </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary tx-13" data-dismiss="modal">Close</button>
             <button type="submit" class="btn btn-primary tx-13"><i data-feather="save" class="wd-10 mg-r-5"></i>
@@ -116,8 +117,8 @@
 </div>
 @endsection
 @section('scripts')
-    <script>
-        $(function(){
+<script>
+  $(function(){
         'use strict'
 
         $('#dataTable').DataTable({
@@ -128,5 +129,5 @@
           }
         });
       });
-    </script>
+</script>
 @endsection
