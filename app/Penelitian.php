9463fc358cc36c51324007cb2fa8b379436a4a88
muhammadasadbastomi <tomycules@gmail.com>
2020-06-01 02:56:32 +0800

    create model penelitian

--- app/Penelitian.php
+++ app/Penelitian.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 
 class Penelitian extends Model
 {
-    //
+    use Uuid;
+
+    public function peneliti()
+    {
+        return $this->belongsTo(Peneliti::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function objek_penelitian()
+    {
+        return $this->belongsTo(Objek_penelitian::class);
+    }
 }
