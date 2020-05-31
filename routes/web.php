128e316c5d75bce76c7582476e285d35ad3354c4
muhammadasadbastomi <tomycules@gmail.com>
2020-06-01 01:55:05 +0800

    route update peneliti

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/peneliti', 'penelitiController@index')->name('penelitiIndex');
     Route::post('/peneliti', 'penelitiController@store')->name('penelitiStore');
     Route::get('/peneliti/detail/{uuid}', 'penelitiController@show')->name('penelitiShow');
-    Route::get('/peneliti/edit', 'penelitiController@edit')->name('penelitiEdit');
+    Route::get('/peneliti/edit/{uuid}', 'penelitiController@edit')->name('penelitiEdit');
+    Route::put('/peneliti/edit/{uuid}', 'penelitiController@update')->name('penelitiUpdate');
 
     //peneliti
     Route::get('/penelitian', 'penelitianController@index')->name('penelitianIndex');
