eda35514e7ea8b7f9e341fb3484a9d50f6cdd92c
muhammadasadbastomi <tomycules@gmail.com>
2020-06-01 00:09:34 +0800

    create verif and preview doc function

--- app/Http/Controllers/permohonanController.php
+++ app/Http/Controllers/permohonanController.php
@@ -2,17 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Permohonan;
 use Illuminate\Http\Request;
 
 class permohonanController extends Controller
 {
     public function index()
     {
-        return view('admin.permohonan.index');
+        $data = Permohonan::orderBy('status', 'asc')->get();
+        return view('admin.permohonan.index', compact('data'));
     }
 
-    public function detail()
+    public function show($uuid)
     {
-        return view('admin.permohonan.detail');
+        $data = Permohonan::where('uuid', $uuid)->first();
+        return view('admin.permohonan.detail', compact('data'));
+    }
+
+    public function lampiranPreview($uuid)
+    {
+        $data = Permohonan::where('uuid', $uuid)->first();
+        $pathToFile = base_path() . '/public/lampiran/permohonan/' . $data->lampiran;
+
+        return response()->file($pathToFile);
+    }
+
+    public function verifikasiPermohonan(Request $request)
+    {
+        $data = Permohonan::where('uuid', $request->uuid)->first();
+        $data->status = $request->status;
+        if ($request->status == 2) {
+            $data->tanggal_pemanggilan = $request->tanggal_pemanggilan;
+        }
+        $data->update();
+
+        return redirect()->back()->with('success', 'Data berhasil diverifikasi');
     }
 }
