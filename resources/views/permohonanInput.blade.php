0cc2b1d60ff8bc3234c06d2030c769be572baef5
muhammadasadbastomi <tomycules@gmail.com>
2020-05-31 23:19:16 +0800

    backend permohonan input

--- resources/views/permohonanInput.blade.php
+++ resources/views/permohonanInput.blade.php
@@ -1,53 +1,56 @@
 @extends('layouts.depan')
 
 @section('content')
-    <section id="cta2">
-        <div class="container">
-            <div class="text-center">
-                <h2 class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms"><span>MULTI</span> IS A CREATIVE HTML TEMPLATE</h2>
-                <img class="img-responsive wow fadeIn" src="{{asset('depan/images/cta2/cta2-img.png')}}" alt="" data-wow-duration="300ms" data-wow-delay="300ms">
-            </div>
+<section id="cta2">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms"><span>MULTI</span> IS A CREATIVE
+                HTML TEMPLATE</h2>
+            <img class="img-responsive wow fadeIn" src="{{asset('depan/images/cta2/cta2-img.png')}}" alt=""
+                data-wow-duration="300ms" data-wow-delay="300ms">
         </div>
-    </section>
-    <section class="">
-        <div class="container" style="padding-left:100px;padding-right:100px;">
-            <div class="">
+    </div>
+</section>
+<section class="">
+    <div class="container" style="padding-left:100px;padding-right:100px;">
+        <div class="">
             <h2 class="text-center">Input Permohonan Penelitian</h2>
             <br>
-            <form action="">
+            <form action="{{Route('permohonanStore')}}" method="POST" enctype="multipart/form-data">
+                @csrf
                 <div class="form-group">
                     <label for="">Nama Lengkap</label>
-                    <input type="text" class="form-control">
+                    <input type="text" name="nama" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="">NIK</label>
-                    <input type="text" class="form-control">
+                    <input type="text" name="NIK" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="">Alamat</label>
-                    <textarea name="alamat" id="" class="form-control"></textarea>
+                    <textarea name="alamat" name="alamat" id="" class="form-control"></textarea>
                 </div>
                 <div class="form-group">
                     <label for="">No Telepon</label>
-                    <input type="text" class="form-control">
+                    <input type="text" name="no_hp" class="form-control">
                 </div>
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="">Tempat Lahir</label>
-                            <input type="text" class="form-control">
+                            <input type="text" name="tempat_lahir" class="form-control">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="">Tanggal Lahir</label>
-                            <input type="date" class="form-control">
+                            <input type="date" name="tanggal_lahir" class="form-control">
                         </div>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="">Pendidikan Terakhir</label>
-                    <select name="pendidikan" id="" class="form-control">
+                    <select name="pendidikan_terakhir" id="" class="form-control">
                         <option value="">-- pilih pendidikan terakhir --</option>
                         <option value="SD">SD</option>
                         <option value="SMP">SMP</option>
@@ -64,27 +67,27 @@
                 </div>
                 <div class="form-group">
                     <label for="">Objek Penelitian</label>
-                    <select name="objekPenelitian_id" id="objekPenelitian_id" class="form-control">
+                    <select name="objek_penelitian_id" id="objek_penelitian_id" class="form-control">
                         <option value="">-- pilih Objek Penelitian</option>
                         @foreach($objekPenelitian as $o)
-                            <option value="{{$o->id}}">{{$o->nama}}</option>
+                        <option value="{{$o->id}}">{{$o->nama}}</option>
                         @endforeach
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="">Keperluan</label>
-                    <textarea name="alamat" id="" class="form-control"></textarea>
+                    <textarea name="keperluan" id="" class="form-control"></textarea>
                 </div>
                 <div class="form-group">
                     <label for="">Lampiran File (Surat Pengantar dll)</label>
-                    <input type="file" class="form-control">
+                    <input type="file" name="lampiran" class="form-control">
                 </div>
                 <div class="text-right">
-                    <button class="btn btn-primary">Kirim Permohonan</button>
+                    <button type="submit" class="btn btn-primary">Kirim Permohonan</button>
                 </div>
             </form>
-            </div>
         </div>
-    </section>
-    <br><br>
+    </div>
+</section>
+<br><br>
 @endsection
