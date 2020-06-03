23605b378622904a7d204250c29fb14eaac5e212
muhammadasadbastomi <tomycules@gmail.com>
2020-06-03 23:20:58 +0800

    revision v2

--- database/migrations/2020_05_31_144259_create_permohonans_table.php
+++ database/migrations/2020_05_31_144259_create_permohonans_table.php
@@ -16,10 +16,11 @@ class CreatePermohonansTable extends Migration
         Schema::create('permohonans', function (Blueprint $table) {
             $table->id();
             $table->string('uuid', 36);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->unsignedBigInteger('objek_penelitian_id');
-            $table->string('nama', 100);
             $table->string('NIK', 50);
             $table->string('alamat', 100);
+            $table->string('email', 50);
             $table->string('no_hp', 13);
             $table->string('tempat_lahir', 50);
             $table->date('tanggal_lahir');
@@ -28,6 +29,7 @@ class CreatePermohonansTable extends Migration
             $table->date('tanggal_pemanggilan')->nullable();
             $table->string('lampiran', 50)->nullable();
             $table->tinyInteger('status')->default(1);
+            $table->text('catatan')->nullable();
             $table->timestamps();
             $table->foreign('objek_penelitian_id')->references('id')->on('objek_penelitians')->onDelete('cascade');
         });
