c4956d0d9809276f15935bc54895e6bc6f4c2287
tri angga utama <trianggategarutama@gmail.com>
2020-06-09 18:50:55 +0800

    vaslidasi username

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\User;
 use Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -47,6 +48,14 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|unique:users',
+        ]);
+    
+        if ($validator->fails()) {
+            return back()->with('warning', 'Username sudah digunakan');
+        }
+
         $user = new User;
         $user->nama = $request->nama;
         $user->username = $request->username;
