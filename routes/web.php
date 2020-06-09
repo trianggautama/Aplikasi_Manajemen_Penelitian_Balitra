9b88876f212efeef091e7f4d2edb687a6a90d2f5
muhammadasadbastomi <tomycules@gmail.com>
2020-06-09 16:55:18 +0800

    delete method jobdesk peneliti

--- routes/web.php
+++ routes/web.php
@@ -129,6 +129,7 @@ Route::group(['middleware' => ['auth']], function () {
     //HALAMAN PENELITI - MENU PENELITIAN
     Route::get('peneliti/penelitian', 'penelitianController@penelitiPenelitianIndex')->name('penelitiPenelitianIndex');
     Route::post('/peneliti/jobdesk/create', 'penelitianController@penelitiJobdeskStore')->name('penelitiJobdeskStore');
+    Route::get('/peneliti/jobdesk/delete', 'penelitianController@penelitiJobdeskDestroy')->name('penelitiJobdeskDestroy');
 
     //HALAMAN PENELITI - MENU jobdesk
     Route::get('peneliti/jobdesk/{uuid}', 'penelitianController@penelitiJobdeskIndex')->name('penelitiJobdeskIndex');
